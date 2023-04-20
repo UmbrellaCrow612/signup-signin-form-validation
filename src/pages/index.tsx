283e4c798d593bb9fc8299e6b3b4cb77f7b95a14
yousaf wazir <umbrellaman612@gmail.com>
2023-04-20 14:13:10 +0100

    sign up page made

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,3 @@
-import {
-  H1,
-  H2,
-  H3,
-  H4,
-  Blockquote,
-  InlineCode,
-  Large,
-  List,
-  Lead,
-  Muted,
-  P,
-  Small,
-} from "@/components/Typography";
-
 export default function Page() {
-  return (
-    <div className="min-h-screen flex items-center justify-center flex-col gap-3">
-      <H1>this is a h1</H1>
-      <H2>H2</H2>
-      <H3>h3</H3>
-      <H4>h4</H4>
-      <Blockquote> Quotes</Blockquote>
-      <InlineCode> Code </InlineCode>
-      <Large> Large</Large>
-      <List>
-        <li>item one</li>
-      </List>
-      <Lead>Lead</Lead>
-      <Muted>Mute</Muted>
-      <P>Para</P>
-      <Small>Small</Small>
-    </div>
-  );
+  return <></>;
 }
---
+++ src/pages/signup.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <form>
+        <Card className="w-[25rem]">
+          <CardHeader>
+            <CardTitle>Sign up</CardTitle>
+            <CardDescription>
+              You need to sign up to access the app
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-8">
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="email">Username</Label>
+              <Input
+                type="text"
+                id="username"
+                placeholder="Username"
+                required
+              />
+            </div>
+
+            <div className="space-y-4">
+              <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor="email">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+
+              <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor="email">Confirm Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="ghost" type="button">
+              Clear
+            </Button>
+            <Button type="submit">Create</Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </main>
+  );
+}
