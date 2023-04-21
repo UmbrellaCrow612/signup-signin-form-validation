22ef88acd268149e91d76eabf9aafe91f26f6d1a
yousaf wazir <umbrellaman612@gmail.com>
2023-04-21 10:04:03 +0100

    added signInForm component

---
+++ src/components/auth/SignInForm.tsx
@@ -0,0 +1,76 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useRouter } from "next/router";
+import { FormEvent } from "react";
+
+export default function SignInForm() {
+  const router = useRouter();
+
+  const pushToSignUp = (e: FormEvent) => {
+    e.preventDefault();
+    router.push("/auth/signup");
+  };
+  return (
+    <>
+      {" "}
+      <form>
+        <Card className="w-[25rem]">
+          <CardHeader>
+            <CardTitle>Sign in</CardTitle>
+            <CardDescription>
+              You need to sign in to access the app
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="email">Username</Label>
+              <Input
+                type="text"
+                id="username"
+                placeholder="Username"
+                required
+              />
+            </div>
+
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="email">Password</Label>
+              <Input
+                type="password"
+                id="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <div className="flex justify-between w-full">
+              <Button variant="ghost" type="button">
+                Clear
+              </Button>
+              <Button type="submit">Sign in</Button>
+            </div>
+            <div className="flex items-center w-full gap-2">
+              <CardDescription>Don&apos;t have an account ?</CardDescription>{" "}
+              <Button
+                variant="link"
+                type="button"
+                onClick={(e) => pushToSignUp(e)}
+              >
+                Sign up
+              </Button>
+            </div>
+          </CardFooter>
+        </Card>
+      </form>
+    </>
+  );
+}
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,75 +1,9 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useRouter } from "next/router";
-import { FormEvent } from "react";
+import SignInForm from "@/components/auth/SignInForm";
 
 export default function Page() {
-  const router = useRouter();
-
-  const pushToSignUp = (e: FormEvent) => {
-    e.preventDefault();
-    router.push("/auth/signup");
-  };
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <form>
-        <Card className="w-[25rem]">
-          <CardHeader>
-            <CardTitle>Sign in</CardTitle>
-            <CardDescription>
-              You need to sign in to access the app
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="email">Username</Label>
-              <Input
-                type="text"
-                id="username"
-                placeholder="Username"
-                required
-              />
-            </div>
-
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="email">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="Password"
-                required
-              />
-            </div>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <div className="flex justify-between w-full">
-              <Button variant="ghost" type="button">
-                Clear
-              </Button>
-              <Button type="submit">Sign in</Button>
-            </div>
-            <div className="flex items-center w-full gap-2">
-              <CardDescription>Don&apos;t have an account ?</CardDescription>{" "}
-              <Button
-                variant="link"
-                type="button"
-                onClick={(e) => pushToSignUp(e)}
-              >
-                Sign up
-              </Button>
-            </div>
-          </CardFooter>
-        </Card>
-      </form>
+      <SignInForm />
     </main>
   );
 }
